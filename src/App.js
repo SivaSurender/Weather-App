1b2ff8c5e671a7a7065a5889c3c254dc961c7d42
SivaSurender <e.surender14@gmail.com>
2023-01-29 19:17:01 +0530

    Added Code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Input from "./components/Input";
 import TopButtons from "./components/TopButtons";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperaturDetails from "./components/TemperaturDetails";
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Input />
+      <TimeAndLocation />
+      <TemperaturDetails />
     </div>
   );
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 28 January 2022 | Local Time: 18:40 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Chennai, IND </p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
